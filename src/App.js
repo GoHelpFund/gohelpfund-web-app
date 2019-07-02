b42115c2300f793a2269007d2e6769e0c3d3516b
Daniel Tirzuman <github@tirzuman.com>
2019-07-02 16:22:10 +0300

    - redirect to /home

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ class App extends Component {
           <CssBaseline />
           <Header isLoggedIn={this.state.isLoggedIn} updateLoginState={this.updateLoginState} />
           <div id="app-content">
-            {/* <Redirect to="/home" component={Home} /> */}
+            <Redirect to="/home" component={Home} />
             <Route path="/home" component={Home} />
             <Route path="/campaign-details" component={CampaignDetails} />
             <Route path="/campaign-details-ant" component={CampaignDetailsAnt} />
