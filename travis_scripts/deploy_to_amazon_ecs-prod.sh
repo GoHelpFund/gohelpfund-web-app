031e1b5bb2235ea5283cc43a865007c046a18816
Daniel Tirzuman <github@tirzuman.com>
2019-09-26 21:48:21 +0300

    refactor ecs cluster name

--- travis_scripts/deploy_to_amazon_ecs-prod.sh
+++ travis_scripts/deploy_to_amazon_ecs-prod.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "Launching $BUILD_NAME IN AWS Elastic Container Service"
 ecs-cli configure profile --profile-name ghf-web-app --access-key $AWS_ACCESS_KEY --secret-key $AWS_SECRET_KEY
-ecs-cli configure --region eu-central-1 --cluster ecs-eu-central-1-ghf-web-app-prod
+ecs-cli configure --region eu-central-1 --cluster prod-gohelpfund-web-app
 ecs-cli compose --file docker/prod-web/docker-compose.yml --ecs-params docker/prod-web/ecs-params.yml up
 rm -rf ~/.ecs
